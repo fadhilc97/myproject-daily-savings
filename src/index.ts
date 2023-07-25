0afd30650552ab3f26021c714b23fcb02dc908b3
Fadhil <fadhil@incit.org>
2023-07-25 21:56:23 +0700

    Setup CORS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express, { Express } from "express";
+import cors from "cors";
 import { savingsRoute } from "./routes";
 
 const port = 3000;
 const app: Express = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use("/", savingsRoute);
 
 app.listen(port, () => {
